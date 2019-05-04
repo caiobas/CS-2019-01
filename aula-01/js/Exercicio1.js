c0a36af5bdc7166bf3de24af3827161fb161e1bc
Caio Garcia Saraiva <caiobas6@hotmail.com>
2019-05-04 04:42:49 -0300

    Teste Exercicio1 rodando

--- aula-01/js/Exercicio1.js
+++ aula-01/js/Exercicio1.js
@@ -1,15 +1,17 @@
+module.exports = Exercicio1;
+
 function Exercicio1(numero) {
             
     if(typeof numero != "number"){
-        return TypeError("Este parâmetro não é um número.");
+        throw new TypeError("Este parâmetro não é um número.");
     }
 
     if(Math.trunc(numero) != numero){
-        return TypeError("Este parâmetro não é um inteiro.");
+        throw new TypeError("Este parâmetro não é um inteiro.");
     }
     
     if(numero < 0 || numero > 9999) {
-        return RangeError("Numero não está entre 0 e 9999.");
+        throw new RangeError("Numero não está entre 0 e 9999.");
     }
     
     const primDez = Math.trunc(numero / 100);
