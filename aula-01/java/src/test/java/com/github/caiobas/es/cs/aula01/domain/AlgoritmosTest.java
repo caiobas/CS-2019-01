f82e84ac6cc3c99a5434eff0efa9f60bf3635b76
Caio Garcia Saraiva <caiobas6@hotmail.com>
2019-05-15 00:27:44 -0300

    JAVA Configuracao pronta

--- aula-01/java/src/test/java/com/github/caiobas/es/cs/aula01/domain/AlgoritmosTest.java
+++ aula-01/java/src/test/java/com/github/caiobas/es/cs/aula01/domain/AlgoritmosTest.java
@@ -1,17 +1,16 @@
 package com.github.caiobas.es.cs.aula01.domain;
 
-import com.github.caiobas.es.cs.aula01.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 public class AlgoritmosTest {
 	
 	@Test
 	public void numeroMenorQueZero() {
-        assertThrows(IllegalArgumentException.class,
-                () -> Algoritmos.propriedade3025(3025));
+        assertEquals(3025, 3025);
+        assertThrows(IllegalArgumentException.class,() -> Algoritmos.propriedade3025(-1));
+        assertThrows(IllegalArgumentException.class,() -> Algoritmos.propriedade3025(10000));
     }
     
 }
