6d0e68e2bfc5878cc3ac2e6703bebc68caf496a0
Caio Garcia Saraiva <caiobas6@hotmail.com>
2019-05-30 20:23:14 -0300

    JAVA Código de MenorTemperatura implementado

--- aula-07/vetor/java/src/main/java/com/github/caiobas/es/cs/aula07/domain/MenorTemperatura.java
+++ aula-07/vetor/java/src/main/java/com/github/caiobas/es/cs/aula07/domain/MenorTemperatura.java
@@ -1,11 +1,29 @@
-/**
- * 
- * 
- * 
- */
+package com.github.caiobas.es.cs.aula07.domain;
 
+import java.util.Arrays;
+
+/**
+ * Classe com o objetivo de encontrar a menor temperatura
+ * em um vetor de valores recebido como argumento.
+ */
 public final class MenorTemperatura {
 
+    private MenorTemperatura() {
 
-    
+    }
+    /**
+     * Função com o objetivo de encontrar a menor temperatura
+     * em um vetor de valores recebido como argumento.
+     * 
+     * @param vetor Vetor que contém temperaturas.
+     * 
+     * @return Retorna a menor temperatura de um vetor.
+     */
+    public static double obterMenorTemperatura(final double[] vetor) {
+        double[] vetorAuxiliar = vetor;
+        Arrays.sort(vetorAuxiliar);
+
+        return vetorAuxiliar[0];
+    }
+
 }
