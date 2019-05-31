985447bc579d499e3f1055a8400bcdbcdffc63eb
Caio Garcia Saraiva <caiobas6@hotmail.com>
2019-05-30 21:16:18 -0300

    JAVA Teste MenorTemperatura implementado

---
+++ aula-07/vetor/java/src/test/java/com/github/caiobas/es/cs/aula07/domain/MenorTemperaturaTest.java
@@ -0,0 +1,14 @@
+package com.github.caiobas.es.cs.aula07.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class MenorTemperaturaTest {
+
+    @Test
+    public void menorTemperatura() {
+        double[] vetor = {10, 9, 8, 7, 6, 5};
+        assertEquals(5, MenorTemperatura.obterMenorTemperatura(vetor));
+    }
+}
