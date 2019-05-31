d367a7c52ab60082ab501394ce6d9510c79266aa
Caio Garcia Saraiva <caiobas6@hotmail.com>
2019-05-30 21:18:27 -0300

    JAVA Codigo SomaImpar implementado

---
+++ aula-07/vetor/java/src/main/java/com/github/caiobas/es/cs/aula07/domain/SomaImpar.java
@@ -0,0 +1,32 @@
+package com.github.caiobas.es.cs.aula07.domain;
+
+/**
+ * Classe com o objetivo de encontrar a menor temperatura
+ * em um vetor de valores recebido como argumento.
+ */
+public final class SomaImpar {
+
+    private SomaImpar() {
+
+    }
+    /**
+     * Função com o objetivo de encontrar a menor temperatura
+     * em um vetor de valores recebido como argumento.
+     * 
+     * @param vetor Vetor que contém temperaturas.
+     * 
+     * @return Retorna a menor temperatura de um vetor.
+     */
+    public static int obterSomaImpar(final int[] vetor) {
+        int somaImpares = 0;
+
+        for(int contador = 0; contador < vetor.length; contador++){
+            if(vetor[contador] % 2 == 0){
+                somaImpares = somaImpares + contador;
+            }
+        }
+
+        return somaImpares;
+    }
+
+}
