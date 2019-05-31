15abff59eb19c1a6be8fdab528862c34f53ae318
Caio Garcia Saraiva <caiobas6@hotmail.com>
2019-05-31 11:06:22 -0300

    JAVA Teste SomaImpar implementado

---
+++ aula-07/vetor/java/src/test/java/com/github/caiobas/es/cs/aula07/domain/SomaImparTest.java
@@ -0,0 +1,15 @@
+package com.github.caiobas.es.cs.aula07.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class SomaImparTest {
+
+    @Test
+    public void somaImpar() {
+        int[] vetor = {10, 9, 8, 7, 6, 5};
+        assertEquals(21, SomaImpar.obterSomaImpar(vetor));
+    }
+    
+}
