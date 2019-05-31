efe2c9b1f1b9eaefb1d35c7a250d23827459c093
Caio Garcia Saraiva <caiobas6@hotmail.com>
2019-05-31 13:43:41 -0300

    JAVA Codigo PalavraFrequente implementado

---
+++ aula-07/vetor/java/src/main/java/com/github/caiobas/es/cs/aula07/domain/PalavraFrequente.java
@@ -0,0 +1,53 @@
+package com.github.caiobas.es.cs.aula07.domain;
+
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Classe com o objetivo de localizar a palavra mais frequente em uma sequência
+ * de caracteres.
+ */
+public final class PalavraFrequente {
+
+    private PalavraFrequente() {
+
+    }
+    /**
+     * Classe com o objetivo de localizar a palavra
+     * mais frequente em uma sequência de caracteres.
+     * 
+     * @param frase String contendo palavras.
+     * 
+     * @return Retorna a palavra mais frequente em uma frase.
+     */
+    public static String obterPalavraFrequente(final String frase) {
+        HashMap<String, Integer> frequencia = new HashMap<String, Integer>();
+
+        String[] palavras = frase.split(" ");
+
+        for(String palavra : palavras) {
+            if(frequencia.containsKey(palavra)){
+                frequencia.put(palavra, frequencia.get(palavra) + 1);
+            }
+            else{
+                frequencia.put(palavra, 1);
+            }
+        }
+
+        String palavra = "";
+        int compara = 0;
+
+        for(String contador : frequencia.keySet()){
+            if(frequencia.get(contador) > compara) {
+                compara = frequencia.get(contador);
+                palavra = contador;
+            }
+        }
+
+        System.out.println(palavra);
+        return palavra;
+    }
+
+}
