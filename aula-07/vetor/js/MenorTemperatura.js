dde36a7528c1d76c315f240b4adecdd68bdb1aa5
Caio Garcia Saraiva <caiobas6@hotmail.com>
2019-06-02 18:17:49 -0300

    JS Documentacao MenorTemperatura implementado

--- aula-07/vetor/js/MenorTemperatura.js
+++ aula-07/vetor/js/MenorTemperatura.js
@@ -1,4 +1,14 @@
-
+/**
+ * Função com o objetivo de encontrar a menor temperatura
+ * em um vetor de valores recebido como argumento.
+ * 
+ * @param {Array} vetor Vetor usado para análise de menor temperatura.
+ * 
+ * @returns {number} Retorna menor temperatura de um vetor.
+ * 
+ * @throws {TypeError} Se o parâmetro não for um vetor.
+ * @throws {TypeError} Se o vetor não conter apenas números.
+ */
 function menorTemperatura(vetor) {
 
     if(!Array.isArray(vetor)) {
