6ea520c9243472503e9ca679dc9b5a267a9d5cda
Caio Garcia Saraiva <caiobas6@hotmail.com>
2019-06-03 22:42:37 -0300

    JS Correcao teste

---
+++ aula-07/vetor/js/test/QuantidadeLetras.test.js
@@ -0,0 +1,10 @@
+const funcao = require("../QuantidadeLetras.js");
+let string = "E = 2\nI = 1\nO = 1\nS = 3\nT = 2\n";
+
+test("caso classico", () => {
+    expect(funcao("TeSte iSso{[")).toBe(string);
+  });
+
+test("Parametro nao e string", () => {
+    expect(() => { funcao(25); }).toThrow(TypeError);
+});
