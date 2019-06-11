4095bc2cd0fc4ca99ee068de6e7ef9609810d7bc
Caio Garcia Saraiva <caiobas6@hotmail.com>
2019-06-11 20:33:48 -0300

    JAVA Zerando checkstyle

---
+++ aula-01/java/src/main/java/com/github/caiobas/es/cs/aula01/domain/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Pacote do repositório "java". Contém uma classe
+ * apenas: {@link com.github.caiobas.es.cs.aula01.domain.Algoritmos}.
+ * Observe que esta classe possui varios algoritmos.
+ *
+ * @see com.github.caiobas.es.cs.aula01.domain.Algoritmos
+ */
+package com.github.caiobas.es.cs.aula01.domain;
