996528e6572eb81e6440f0e280efc544ae3c42cd
Caio Garcia Saraiva <caiobas6@hotmail.com>
2019-06-22 03:25:12 -0300

    Configurando aula10

---
+++ aula-10/src/main/java/com/github/caiobas/es/cs/aula10/application/console/package-info.java
@@ -0,0 +1,17 @@
+/**
+ * Pacote do repositório "java". Contém seis classes:
+ * {@link com.github.caiobas.es.cs.aula07.domain.ElementosIguais}.
+ * {@link com.github.caiobas.es.cs.aula07.domain.MenorTemperatura}.
+ * {@link com.github.caiobas.es.cs.aula07.domain.NumeroFrequente}.
+ * {@link com.github.caiobas.es.cs.aula07.domain.PalavraFrequente}.
+ * {@link com.github.caiobas.es.cs.aula07.domain.QuantidadeLetras}.
+ * {@link com.github.caiobas.es.cs.aula07.domain.SomaImpar}.
+ *
+ * @see com.github.caiobas.es.cs.aula07.domain.Algoritmos
+ * @see com.github.caiobas.es.cs.aula07.domain.MenorTemperatura
+ * @see com.github.caiobas.es.cs.aula07.domain.NumeroFrequente
+ * @see com.github.caiobas.es.cs.aula07.domain.PalavraFrequente
+ * @see com.github.caiobas.es.cs.aula07.domain.QuantidadeLetras
+ * @see com.github.caiobas.es.cs.aula07.domain.SomaImpar
+ */
+package com.github.caiobas.es.cs.aula07.domain;
