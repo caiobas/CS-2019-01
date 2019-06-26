6c23debfee09c9d085a280795a4539df350ce999
Caio Garcia Saraiva <caiobas6@hotmail.com>
2019-06-25 21:52:45 -0300

    JAVA Correcao inteiro aula08

--- aula-08/inteiro/src/main/java/com/github/caiobas/es/cs/aula08/domain/InteiroUtils.java
+++ aula-08/inteiro/src/main/java/com/github/caiobas/es/cs/aula08/domain/InteiroUtils.java
@@ -2,6 +2,7 @@ package com.github.caiobas.es.cs.aula08.domain;
 
 import java.io.DataInputStream;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
@@ -30,12 +31,10 @@ public final class InteiroUtils {
      * @return Retorna os 4 primeiros bytes
      * do arquivo passado como parâmetro.
      *
-     * @throws IOException Se houver problema ao ler o caminho do arquivo.
      * @throws IllegalArgumentException Se o arquivo não existir
      * ou se houverem dados insuficientes no arquivo.
      */
-    public static String imprimeHexa(final String caminho)
-    throws IOException {
+    public static String imprimeHexa(final String caminho) {
 
         final File arquivo = new File(caminho);
 
@@ -55,15 +54,21 @@ public final class InteiroUtils {
                 "Dados insuficientes no arquivo.");
         }
 
-        final InputStream fis = Files.newInputStream(Paths.get(caminho));
-        final DataInputStream dis = new DataInputStream(fis);
+        try (final InputStream fis = Files.newInputStream(Paths.get(caminho)); 
+            final DataInputStream dis = new DataInputStream(fis)) {
+                
+                final int[] valor = {dis.readByte(), dis.readByte(), dis.readByte(), dis.readByte()};
+                final StringBuilder hexadecimal = new StringBuilder();
+                hexadecimal.append(Integer.toHexString(valor[0])); 
+                hexadecimal.append(Integer.toHexString(valor[1])); 
+                hexadecimal.append(Integer.toHexString(valor[2])); 
+                hexadecimal.append(Integer.toHexString(valor[3])); 
+    
+            return hexadecimal.toString();
+        } catch(IOException io) {
+            throw new IllegalArgumentException("Houve um problema na leitura do arquivo.");
+        }
 
-        final int hexadecimal = dis.readInt();
-
-        fis.close();
-        dis.close();
-
-        return Integer.toHexString(hexadecimal);
     }
 
 }
